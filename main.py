2a2dc27935f71820b8328bb3ce0af1844730f931
ArtursVanags221RDB172 <124793162+ArtursVanags221RDB172@users.noreply.github.com>
2023-02-19 16:32:06 +0000

    Finished assignment

--- main.py
+++ main.py
@@ -14,17 +14,43 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
-            pass
-
+           opening_brackets_stack.append(Bracket(next, i))
         if next in ")]}":
             # Process closing bracket, write your code here
-            pass
+            if not opening_brackets_stack:
+                return i+1
+
+
+               
+            nig = opening_brackets_stack.pop()
+            if not are_matching(nig.char, next):
+                return i+1
+            
+    if  opening_brackets_stack:
+        nig = opening_brackets_stack.pop()
+        return nig.position + 1
+
+    return "Success"
 
 
 def main():
-    text = input()
-    mismatch = find_mismatch(text)
-    # Printing answer, write your code here
+    select_input = input("input - F or I")
+    if select_input.upper() == "F":
+        file_path = input("choose file (input path)")
+        with open(file_path, "r") as f:
+            text = f.read()
+            mismatch = find_mismatch(text)
+            if mismatch == "Success":
+                 print("Success")
+            else:
+                 print(mismatch)
+    else:
+        text = input()
+        mismatch = find_mismatch(text)
+        if mismatch == "Success":
+            print("Success")
+        else:
+            print(mismatch)
 
 
 if __name__ == "__main__":
